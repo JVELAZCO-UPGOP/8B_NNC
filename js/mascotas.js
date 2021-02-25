bf96293cfa966814a004fb3c220be5c3da77eee9
Noraly Correa <n_oraly99@hotmail.com>
2021-02-25 02:20:27 -0600

    Parte 1 Completada

---
+++ js/mascotas.js
@@ -0,0 +1,105 @@
+const listaMascotas = document.getElementById('Lista-mascotas');
+const tipo = document.getElementById('tipo');
+const nombre = document.getElementById('nombre');
+const dueno = document.getElementById('dueno');
+const form = document.getElementById('form');
+const btnGuardar = document.getElementById('btn-guardar');
+const indice = document.getElementById('indice');
+
+let mascotas = [
+    {
+        tipo: "Perro",
+        nombre: "Luna",
+        dueno: "Nora"
+    },
+
+    {
+        tipo: "Gato",
+        nombre: "Aly",
+        dueno: "Karen"
+    }
+
+];
+
+
+function listarMascotas() {
+    const htmlMascotas = mascotas.map((mascota, index)=>`<tr>
+    <th scope="row">${index}</th>
+    <td>${mascota.tipo}</td>
+    <td>${mascota.nombre}</td>
+    <td>${mascota.dueno}</td>
+    <td>
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-info editar"data-bs-toggle="modal" data-bs-target="#staticBackdrop" ><i class="far fa-edit"></i></button>
+        <button type="button" class="btn btn-danger eliminar"><i class="far fa-trash-alt"></i></button>
+    </div>
+    </td>
+</tr>`).join("");
+
+listaMascotas.innerHTML =htmlMascotas;
+Array.from(document.getElementsByClassName('editar')).forEach((botonEditar, index)=> botonEditar.onclick = editar(index));
+//Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar, index)=> botonEliminar.onclick = eliminar(index));
+
+
+}
+
+
+function enviardatos(evento){
+    
+    evento.preventDefault();
+    const datos={
+        tipo: tipo.value,
+        nombre: nombre.value,
+        dueno: dueno.value
+    };
+    const accion = btnGuardar.innerHTML;
+    switch(accion){
+        case 'Editar':
+            mascotas[indice.value]=datos;
+            break;
+            default:
+                mascotas.push(datos);
+                break;
+    }
+    
+    
+    listarMascotas();
+    reserModal();
+}
+
+function editar(index) {
+    return function handler(){
+        btnGuardar.innerHTML='Editar'
+        $('#staticBackdrop').modal('toggle');
+        const mascota = mascotas[index];
+        nombre.value = mascota.nombre;
+        dueno.value = mascota.dueno;
+        tipo.value = mascota.tipo;
+        indice.value=index;
+        
+
+    }
+}
+
+function reserModal(){
+    nombre.value = '';
+    dueno.value = '';
+    tipo.value = '';
+    indice.value='';
+    btnGuardar.innerHTML='Crear';
+}
+
+/*function eliminar(index){
+    return function clickEliminar(){
+        mascotas = mascotas.filter(mascota, indiceMascota)=> indiceMascota !== index); 
+        listarMascotas();
+    }
+    
+
+}*/
+
+listarMascotas();
+
+form.onsubmit = enviardatos;
+btnGuardar.onclick = enviardatos;
+
