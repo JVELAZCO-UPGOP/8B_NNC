f9396ebc48e997d98aced9186d13ec4d912df237
Noraly Correa <n_oraly99@hotmail.com>
2021-03-09 23:18:17 -0600

    Se agrego los modulos enrutador y request-handler

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const enrutador = require("./enrutador");
+const requestHandler = require("./request-handler");
 
-let recursos ={
+global.recursos ={
     mascotas: [
         {tipo: "Perro", nombre: "Lus", dueno: "Nora"},
         {tipo: "Gato", nombre: "Miss", dueno: "Melissa"},
@@ -12,103 +14,7 @@ let recursos ={
     ],
 };
 
-const callbackDelServidor= (req, res) => {
-    //Obtener url desde el objeto request
-    const urlActual = req.url;
-    const urlParseada = url.parse(urlActual, true);
-
-    //Obtener la ruta
-    const ruta = urlParseada.pathname;
-    const rutaLimpia = ruta.replace(/^\/+|\/+$/g, "");
-
-   // obtener metodo http
-    const metodo = req.method.toLowerCase();
-
-    //Obtener las variables del query url
-    
-    const {query = {}}=urlParseada;
-
-    //Obtener los headers
-    const {headers = {}}=req;
-    //Obtener pay en el caso de haber uno
-    const decoder= new StringDecoder('utf-8');
-    let buffer = "";
-
-    //Ir acumulando la data cuando el request reciba un payload
-    req.on('data', (data) => {
-        buffer += decoder.write(data);
-    });
-    //terminar de acumunar datos y finalizarlo
-    req.on('end', () => {
-        buffer += decoder.end();
-
-        if (headers["content-type"] === "application/json") {
-            buffer = JSON.parse(buffer);
-        }
-
-          //3.4.3 revisar si tiene subrutas en este caso es el indice del array
-        let indice = null;
-        if (rutaLimpia.indexOf("/") > -1) {
-            indice = rutaLimpia.split("/");
-            rutaLimpia = indice[0];
-            indice = indice[1];
-        }
-
-        //ordenar data
-        const data ={
-            ruta: rutaLimpia,
-            query,
-            metodo,
-            headers,
-            payload: buffer
-        };
-
-        console.log({data});
-        //Elegir el manejador de la ruta y asignar la funcion que el enrutador tiene
-        let handler;
-        if(ruta && enrutador[rutaLimpia] && enrutador[rutaLimpia][metodo]){
-            handler = enrutador[rutaLimpia][metodo];
-        }
-        else{
-            handler=enrutador.noEncontrado;
-        }
-
-        //ejecutar handler
-        if(typeof handler === 'function'){
-            handler(data, (statusCode=200, mensaje) => {
-                const respuesta = JSON.stringify(mensaje);
-                res.setHeader("Content-Type", "application/json");
-                res.writeHead(statusCode);
-
-                //linea donde se esta respondiendo a la aplicacion cliente
-                res.end(respuesta);
-            });
-        }
-
-    });
-
-};
-
-const enrutador = {
-    ruta: (data, callback) => {
-        callback(200, { mensaje: 'esta es /ruta'});
-    },
-    mascotas: {
-        get: (data, callback) => {
-        callback(200, recursos.mascotas );
-    },
-    post: (data, callback) => {
-        recursos.mascotas.push(data.payload);
-        callback(201, data.payload);
-    },
-    },
-    noEncontrado: (data, callback) => {
-        callback(404, {mensaje: 'no encontrado' });
-    },
-};
-
-
-const server = http.createServer(callbackDelServidor);
+const server = http.createServer(requestHandler);
 
 server.listen(5000 , ()=>{
     console.log('El servidor esta escuchando peticiones en http://localhost:5000/');
